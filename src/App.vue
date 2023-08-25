d20bf2590d6e98e2a634ea8756eace46dd9c032e
darronj <darron.jennings@alloy.digital>
2023-08-25 10:45:28 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MyComp from './components/MyComp.vue';
+</script>
+
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <h1 class="text-3xl font-bold">My Stuff</h1>
+    <div class="flex flex-row">
+      <MyComp />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
